2c7bdb79befd40df8e50aad59842c20389b0bbb9
themohammadsa <59393936+themohammadsa@users.noreply.github.com>
2021-05-07 22:02:20 +0530

    initial commit from repl

---
+++ Routes/products.route.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Product } = require("../Models/product.model.js");
+const router = express.Router();
+
+router.route("/")
+.get(async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({success: true, products});
+  } catch (err) {
+    res.status(500).json({  
+      success: false, error: "Unable to get Products from server!"
+    })
+  }  
+})
+
+// router.route("/:id")
+// .get(async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findById(id);
+//     res.json(product);
+//   } catch (err) {
+//       res.status(500).json({  
+//       success: false, error: "Unable to get Products from server!"
+//     })
+//   }
+// });
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require("mongoose")
+const app = express();
+var cors = require('cors')
+
+const productsRouter = require("./Routes/products.route")
+const { addToDatabase } = require("./Database/fakerDatabase.js")
+const { databaseConnection } = require("./Database/databaseConnection.connect.js")
+const PORT = 3000;
+
+app.use(cors());
+
+
+databaseConnection();
+
+// addToDatabase();
+
+app.use("/products", productsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+
+app.listen(PORT, () => {
+  console.log('server started on port: ', PORT);
+});
